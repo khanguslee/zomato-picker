c4b5003e38b794e0e654e2e82d70865e4edeb54d
khanguslee <khanguslee@gmail.com>
2020-04-10 23:55:35 +1000

    Add basic header to page

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+import "./Header.css";
+
+const Header: FunctionComponent = () => {
+  return (
+    <div className="HeaderContainer">
+      <h1>Zomato Picker</h1>
+    </div>
+  );
+};
+
+export default Header;
